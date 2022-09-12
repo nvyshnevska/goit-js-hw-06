9c5d40d5acd2349ce23fbe66e838c1315b59d127
Natalia Vyshnevska <nvyshnevska@tutanota.com>
2022-09-12 10:35:00 +0200

    hw finished

--- js/task-10.js
+++ js/task-10.js
@@ -7,33 +7,26 @@ const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const boxesRef = document.querySelector("#boxes");
 
-const onInput = (event) => {
-  let amount = 0;
-  amount = event.target.value;
+createBtn.addEventListener("click", () => createBoxes(Number(inputRef.value)));
 
+function createBoxes(amount) {
   const boxesList = [];
 
-  for (let i = 0; i <= amount; i += 1) {
+  console.log(amount);
+
+  for (let i = 0; i < amount; i += 1) {
     const boxEl = document.createElement("div");
     boxEl.style.backgroundColor = getRandomHexColor();
     boxEl.style.width = `${30 + i * 10}px`;
     boxEl.style.height = `${30 + i * 10}px`;
     boxesList.push(boxEl);
   }
-};
 
-inputRef.addEventListener("input", onInput);
+  boxesRef.append(...boxesList);
+}
 
-boxesRef.append(...boxesList);
+destroyBtn.addEventListener("click", destroyBoxes);
 
-// function createBoxes(amount) {
-//   for (let i = 0; i <= amount; i += 1) {
-//     const boxEl = document.createElement("div");
-//     boxEl.style.backgroundColor = getRandomHexColor();
-//     boxEl.style.width = `${30 + i * 10}px`;
-//     boxEl.style.height = `${30 + i * 10}px`;
-//     boxesList.push(boxEl);
-//   }
-// }
-
-// createBoxes.addEventListener("click", createBoxes(amount));
+function destroyBoxes(event) {
+  boxesRef.innerHTML = "";
+}
